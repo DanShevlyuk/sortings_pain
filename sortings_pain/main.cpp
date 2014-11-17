41ca57f1258764d5126411d2a8fc5d000293ba6e
Dan Shevlyuk <twistedplane@gmail.com>
2014-11-17 11:20:56 +0300

    Initial commit

---
+++ sortings_pain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  sortings_pain
+//
+//  Created by Dan Shevlyuk on 11/17/14.
+//  Copyright (c) 2014 Dan Shevlyuk. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
